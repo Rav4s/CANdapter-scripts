d0a168746a486dba4ee2e29d19877a97eefa37c2
Ravi Shah <ravidhruvishshah@gmail.com>
2025-01-30 21:18:03 -0600

    fix loop and output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# TODO: add file output/logging functionality
+# TODO: #1 add file output/logging functionality
 
+import sys
 import can
 import pyCandapter
 import signal
@@ -32,26 +33,21 @@ if device == "0":
     mppt_id = input("Enter the MPPT Device ID (Press enter for default): ")
     if mppt_id == "":
         mppt_id = DEFAULT_MPPT_ID
-    mppt_id = int(mppt_id, 16) # convert to int cause python is ew
-    
-    # message to test if translation is working (remove for prod)
-    # test_message = can.Message(arbitration_id=0x200, data=[0x02, 0xB7, 0xFF, 0x8D, 0x0C, 0x8C, 0xFF, 0xCD], is_extended_id=False)
-    
-    # test_message = can.Message(arbitration_id=0x201, data=[0x02, 0x00, 0x00, 0x17, 0x17], is_extended_id=False)
-
-    test_string = mppt_data_readable(mppt_id, test_message)
-    print(test_string)
-    
+    mppt_id_int = int(mppt_id, 16) # convert to int cause python is ew
+    print(f"\n\n\nCAN frames for MPPT at {mppt_id}:")
     # loop to read CAN messages
-    '''
     while True:
-        print(chr(27) + "[2J") # clear the console (maybe better to not have this?)
-        print(f"CAN frames for MPPT at {mppt_id}:")
-            message = candapter.readCANMessage()
-            if message is not None:
-            print(message)
-        print("message")
-        time.sleep(1)'''
+        # print(chr(27) + "[2J") # clear the console (maybe better to not have this?)
+        '''message = candapter.readCANMessage()
+        if message is not None:
+            print(mppt_data_readable(mppt_id, message))'''
+            
+        # messages to test if translation is working (remove for prod)
+        test_message = can.Message(arbitration_id=0x200, data=[0x02, 0xB7, 0xFF, 0x8D, 0x0C, 0x8C, 0xFF, 0xCD], is_extended_id=False)
+        # test_message = can.Message(arbitration_id=0x201, data=[0x02, 0x00, 0x00, 0x17, 0x17], is_extended_id=False)
+        test_string = mppt_data_readable(mppt_id_int, test_message)
+        print(test_string)
+        time.sleep(0.5)
         
 elif device == "1":
     print("CAN frames for Motor Controller:")
